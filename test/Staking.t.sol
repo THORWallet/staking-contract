19042791dc0f68858d1faed7547c6358cfff91fa
Stevan Bogosavljevic <stevyhacker@gmail.com>
2024-05-07 13:47:35 +0200

    Update staking test setup

--- test/Staking.t.sol
+++ test/Staking.t.sol
@@ -6,32 +6,43 @@ import {TGTStakingBasic} from "../contracts/TGTStakingBasic.sol";
 import {USDC} from "../contracts/mocks/USDC.sol";
 import {MockTGT} from "../contracts/mocks/MockTGT.sol";
 import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+import {IERC20Upgradeable} from "../contracts/libraries/IERC20Upgradeable.sol";
 
 contract StakingTest is Test {
     TGTStakingBasic public tgtStaking;
     USDC public rewardToken;
     MockTGT public tgt;
+    address stakingProxy;
 
     function setUp() public {
 
         rewardToken = new USDC();
         tgt = new MockTGT();
 
-        tgtStaking = Upgrades.deployTransparentProxy(
+        stakingProxy = Upgrades.deployTransparentProxy(
             "TGTStakingBasic.sol",
             msg.sender,
-            abi.encodeCall(TGTStakingBasic.initialize, (rewardToken, tgt))
+            abi.encodeCall(TGTStakingBasic.initialize, (IERC20Upgradeable(address(tgt), IERC20Upgradeable(address(rewardToken))), msg.sender, 0))
         );
 
+        tgtStaking = TGTStakingBasic(stakingProxy);
+
+        rewardToken.mint(msg.sender, 1000e6);
+        address [] memory addresses = new address[](2);
+        uint96 [] memory amounts = new uint96[](2);
+        addresses[0] = msg.sender;
+        addresses[1] = address(this);
+        amounts[0] = 500e6;
+        amounts[1] = 500e6;
+
+        tgt.mint(addresses, amounts);
+        tgt.mintFinish();
     }
 
-//    function test_Increment() public {
-//        counter.increment();
-//        assertEq(counter.number(), 1);
-//    }
-//
-//    function testFuzz_SetNumber(uint256 x) public {
-//        counter.setNumber(x);
-//        assertEq(counter.number(), x);
-//    }
+    function test_Deposit() public {
+        tgt.approve(address(tgtStaking), 1e6);
+        tgtStaking.deposit(1e6);
+    }
+
+
 }
